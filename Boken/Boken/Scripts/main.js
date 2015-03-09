feed418630176366ff42786004dbd99ff089667d
FilleJ <fille_jonsson@hotmail.com>
2015-03-09 12:20:40 +0100

    modaltesting

--- Boken/Boken/Scripts/main.js
+++ Boken/Boken/Scripts/main.js
@@ -1,1 +1,11 @@
-//nothing to see here, move along citizen...
+$(function () {
+    $(document).on("click", "header #loginButton", function () {
+       
+        showModal($(this));
+    });
+});
+
+//function to display a bootstrap modal
+function showModal(clickedBtn) {
+    $("body > .modal").modal("show");
+}
