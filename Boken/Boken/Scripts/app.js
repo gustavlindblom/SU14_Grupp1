6c69976f38720ed6d2f19417d634b52c7c45d4e1
niklasstromberg <niklasstromberg1@gmail.com>
2015-03-09 13:54:29 +0100

    Added routeproviders for booklist, genrelist & authorlist .html + controllers

--- Boken/Boken/Scripts/app.js
+++ Boken/Boken/Scripts/app.js
@@ -9,6 +9,18 @@ app.config(["$routeProvider", "$locationProvider", function($routeProvider, $loc
       templateUrl: "partials/home.html",
       controller: "homeController"
     })
+    .when("/books", {
+        templateUrl: "partials/booklist.html",
+        controller: "booklistController"
+    })
+    .when("/genres", {
+        templateUrl: "partials/genrelist.html",
+        controller: "genrelistController"
+    })
+    .when("/authors", {
+        templateUrl: "partials/authorlist.html",
+        controller: "authorlistController"
+    })
     .when("/add-book", {
         templateUrl: "partials/bookform.html",
         controller: "bookformController"
