77970a7e8fb12bc86a3cb8f36d1ebb4e75b2f6fd
mikaelsaras <mikael.saras@gmail.com>
2015-03-10 13:09:19 +0100

    ggl

--- Boken/Boken/Application/genrelistController.js
+++ Boken/Boken/Application/genrelistController.js
@@ -6,9 +6,27 @@
         $scope.output = JSON.stringify(data, null, '\t');
         $scope.genres = data;
     });
+    
+
+    $scope.showGenre = function (genre) {
+        
+        var name = genre{}
+        
+        console.log(name);
+
+        
+
+        //data is a string (value of textarea)
+        var data = $scope.callData;
+
+        //if user did not pick a url && method, abort
+        if (!$scope.callUrl || !$scope.callMethod) {
+            // if something is missing, notify user
+            $scope.restMessage = "Please fill in all required fields!";
+            return;
+        }
+    };
+
     Genres.get();
 
-
-
 }]);
-
