7171cf5470a722753b9ac98317945b56ed62ff03
niklasstromberg <niklasstromberg1@gmail.com>
2015-03-11 14:38:37 +0100

    Added function filterBooksByAuthor in booklistController.cs

--- Boken/Boken/Application/booklistController.js
+++ Boken/Boken/Application/booklistController.js
@@ -1,6 +1,6 @@
 ﻿app.controller("booklistController", ["$scope", "Books", function ($scope, Books) {
     console.log("books loaded");
-    
+
     $scope.$on("gotBooks", function (event, data) {
         console.log("gotBooks triggered : ", data);
         $scope.output = JSON.stringify(data, null, '\t');
@@ -12,9 +12,32 @@
         $scope.textModal = book;
 
     };
-     Books.get();
 
-     
+    // authorId hämtas från fritextfältet, genreId hämtas ifrån DD, data är inlästa listan av böcker
+    $scope.filterBooksByAuthor = function (authorId, genreId, data) {
+        var booksByAuthor = [];
+
+        for (var book in data) {
+            for (var author in book.Authors) {
+                if (author.Id === authorId) {
+                    if (genreId) {
+                        for (var genre in book.Genres)
+                        {
+                            if (genre.Id === $scope.selectedGenre) {
+                                booksByAuthor.push(book);
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        return booksByAuthor;
+    };
+
+
+    Books.get();
+
+
 }]);
 
 
