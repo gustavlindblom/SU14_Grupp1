194ef7ab9af83da0e5cfc3bee169891f41f9ff3a
mikaelsaras <mikael.saras@gmail.com>
2015-03-11 21:43:44 +0100

    modal

--- Boken/Boken/Application/genrelistController.js
+++ Boken/Boken/Application/genrelistController.js
@@ -1,4 +1,4 @@
-﻿app.controller("genrelistController", ["$scope", "Genres", function ($scope, Genres) {
+﻿app.controller("genrelistController", ["$scope", "Genres", "$modal", "$log", function ($scope, Genres, $modal, $log) {
     console.log("genrelistController is working!");
 
     $scope.$on("gotGenres", function (event, data) {
@@ -8,7 +8,52 @@
     });
     
 
-    $scope.showGenre = function (genre) {
+    //$scope.open = function (size) {
+
+    //    var modalInstance = $modal.open({
+    //        templateUrl: "Partials/modal.html",
+    //        controller: "modalController",
+    //        size: size,
+    //         resolve: {
+    //            items: function () {
+    //                //return $scope.items;
+    //            }
+    //        }
+    //    });
+
+    //        modalInstance.result.then(function (selectedItem) {
+    //        $scope.selected = selectedItem;
+    //    }, function () {
+    //        $log.info('Modal dismissed at: ' + new Date());
+    //    });
+    //};
+
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.open = function (size) {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'Partials/modal.html',
+            controller: 'modalController',
+            size: size,
+            resolve: {
+                items: function () {
+                    return $scope.items;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
+
+
+
+    $scope.showGenre = function () {
         $scope.textModal = genre;
         };
 
---
+++ Boken/Boken/Application/modalController.js
@@ -0,0 +1,23 @@
+﻿app.controller("modalController", ["$scope", "$modalInstance", function ($scope, $modalInstance) {
+    console.log("modalControsadfsadller is alive!");
+
+    //$scope.ok = function () {
+    //    $modalInstance.close("meh");
+    //};
+
+    //$scope.cancel = function () {
+    //    $modalInstance.dismiss('cancel');
+    //};
+    
+
+
+}]);
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+    console.log("modal modalController is alive!");
+
+
+});
