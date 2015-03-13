fb2285b31e6da379ee0804fa94a6a5029e8e61a1
mikaelsaras <mikael.saras@gmail.com>
2015-03-13 11:25:29 +0100

    .

---
+++ Boken/Boken/Application/authorModalController.js
@@ -0,0 +1,20 @@
+﻿app.controller("authorModalController", ["$scope", "Authors", "$modalInstance", "id", function ($scope, Authors, $modalInstance, id) {
+
+$scope.$on("gotAuthor", function (event, data) {
+    console.log("modal controller: ", data);
+    $scope.author = data;
+});
+
+$scope.ok = function () {
+
+    $modalInstance.close();
+};
+
+$scope.cancel = function () {
+
+    $modalInstance.dismiss('cancel');
+};
+
+Authors.get(id);
+
+}]);
