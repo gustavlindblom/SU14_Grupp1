6812d227ab6e3dc5d045b3f57552f104b5ce572a
FilleJ <fille_jonsson@hotmail.com>
2015-03-13 10:43:51 +0100

    author

--- Boken/Boken/Application/authorlistController.js
+++ Boken/Boken/Application/authorlistController.js
@@ -1,4 +1,4 @@
-﻿app.controller("authorlistController", ["$scope", "Authors", function ($scope, Authors) {
+﻿app.controller("authorlistController", ["$scope", "Authors", "$modal", "$log", function ($scope, Authors, $modal, $log) {
     console.log("authors loaded");
 
     $scope.$on("gotAuthors", function (event, data) {
@@ -13,6 +13,31 @@
         $scope.textModal = author;
         
     };
+    // ----- Modal ----- //
+    $scope.open = function (author) {
+        var modalInstance = $modal.open({
+            templateUrl: 'partials/authorModal.html',
+            controller: 'modalController',
+            //size: size, 
+            resolve: {
+                items: function () {
+
+                    return author;
+                }
+            }
+        });
+
+
+        modalInstance.result.then(function (selectedItem) {
+
+
+            $scope.selected = selectedItem;
+        }, function () {
+
+        });
+    };
+    //----------------------------//
+
     Authors.get();
 }]);
 
