7a1b9403a295884d411f8fa6b4baf00f7e2040d6
FilleJ <fille_jonsson@hotmail.com>
2015-03-18 14:32:17 +0100

    skapa bok view som har html för att lägga till flera genres och authors samtidigt - ej funktionalitet

--- Boken/Boken/Application/createBookModalController.js
+++ Boken/Boken/Application/createBookModalController.js
@@ -1,7 +1,13 @@
 ﻿app.controller("createBookModalController", ["$scope", "$modalInstance", function ($scope, $modalInstance) {
 
-   
- 
+
+    $scope.addGenre = function () {
+        document.getElementById('addGenreId').innerHTML += '<input class="input-group" placeholder="Genre..." style="font-style:italic;">';
+    };
+    
+    $scope.addAuthor = function () {
+        document.getElementById('addAuthorId').innerHTML += '<input class="input-group" placeholder="Namn..." style="font-style:italic;">';
+    };
 
     $scope.ok = function () {
 
