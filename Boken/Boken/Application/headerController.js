760851333706de7526aef80277812400f61c8a7e
niklasstromberg <niklasstromberg1@gmail.com>
2015-03-18 10:36:17 +0100

    Logga in ändrar text efter vad man är inloggad som

--- Boken/Boken/Application/headerController.js
+++ Boken/Boken/Application/headerController.js
@@ -2,18 +2,21 @@
     $scope.GoTo = function (url) {
         $location.url(url);
     }
+    $rootScope.DDtext = "Logga in";
     $rootScope.loggedin = "";
     $rootScope.loginStatus = function (id) {
         if (id == 1) {
             
             $rootScope.loggedin = [id];
+            $rootScope.DDtext = "Admin";
             console.log("Admin: " + $rootScope.loggedin);
             return $rootScope.loggedin;
         }
         else if (id == 2) {
             
             $rootScope.loggedin = [id];
-            console.log("Slav: " + $rootScope.loggedin);
+            $rootScope.DDtext = "Lager"
+            console.log("Lager: " + $rootScope.loggedin);
             return $rootScope.loggedin;
         }
     };
