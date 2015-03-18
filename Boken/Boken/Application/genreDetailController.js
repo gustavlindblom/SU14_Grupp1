15b2b0cc28cb2ac7036c663c86a26af04dcd6ea7
mikaelsaras <mikael.saras@gmail.com>
2015-03-18 15:51:56 +0100

    kod för detaljvyn för genre

--- Boken/Boken/Application/genreDetailController.js
+++ Boken/Boken/Application/genreDetailController.js
@@ -1,19 +1,45 @@
 app.controller("genreDetailController", ["$scope", "Genres", "$modalInstance", "id", function ($scope, Genres, $modalInstance, id) {
     console.log("")
+
+    
     $scope.$on("gotGenre", function (event, data) {
         console.log("modal controller: ", data);
-        $scope.genre = data;
+        $scope.genre = $scope.orgGenre = data;
+        console.log("Skriver ut ny genre:", $scope.orgGenre)
     });
+    $scope.orgGenre = angular.copy($scope.genre)
+    
+    
 
-    $scope.ok = function () {
+    $scope.save = function () {
+
+        var orgGenre = $scope.orgGenre
+        console.log("skriver ut från save orgGenre id: ", orgGenre.Id, "objektet: ", orgGenre)
         
+        //console.log("skriver ut vad jag skíckar:", Genres.put(orgGenre.Id, orgGenre));
+
+
+
         $modalInstance.close();
     };
     
     $scope.cancel = function () {
-        
+        console.log("Kommer jag till cancel?");
         $modalInstance.dismiss('cancel');
     };
 
     Genres.get(id);
+
+    $scope.editView = "0";
+    $scope.editStatus = function (id) {
+            if (id == 1) {
+                $scope.editView = [id];
+                console.log("edit id:1: " + $scope.editView);
+                }
+            if (id == 0) {
+                $scope.editView = [id];
+                console.log("edit id:0: " + $scope.editView);
+               }
+    };
+
 }]);
