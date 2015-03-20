73df5dc3bf574741c06db3f3aa740db905c2a3d4
mikaelsaras <mikael.saras@gmail.com>
2015-03-20 10:39:28 +0100

    bugfixt och början på att skapa ny genre

--- Boken/Boken/Application/headerController.js
+++ Boken/Boken/Application/headerController.js
@@ -5,6 +5,7 @@
         $location.url(url);
     }
 
+    // ------- Öppna vy för att skapa bok ---- //
     $scope.openCreateBook = function () {
 
         var modalInstance = $modal.open({
@@ -22,7 +23,37 @@
 
         });
     };
-   
+    // --------------------------------------//
+
+    // ----- öppna vy för att skapa ny genre--- //
+
+    //$scope.createNewGenre = function ( view) {
+    //    //console.log("genre", genre, "view", view);
+    //    var modalInstance = $modal.open({
+    //        templateUrl: 'partials/genreDetail.html',
+    //        controller: 'genreDetailController',
+    //        //size: size,
+    //        resolve: {
+    //            param: function () {
+    //                params = {
+    //                    view: view
+    //                }
+    //                console.log("param:", params)
+    //                return params;
+    //            }
+    //        }
+    //    });
+
+    //    modalInstance.result.then(function (selectedItem) {
+    //        console.log("Kommer vi hit någon  gång gång?");
+    //        Genres.get();
+    //        $route.reload();
+    //        $scope.selected = selectedItem;
+    //    }, function () {
+
+    //    });
+    //};
+    //---------Slut ---- -------------------//
 }
 ]);
 
