974c1b6b40f8a4f7fac4cbed5ab550af6b3a0f23
mikaelsaras <mikael.saras@gmail.com>
2015-03-21 20:48:33 +0100

    uppdaterad genres filer där  utsende och responsivitet är fixad, samt logik flyttat till controlern från html

--- Boken/Boken/Application/genreDetailController.js
+++ Boken/Boken/Application/genreDetailController.js
@@ -2,11 +2,11 @@ app.controller("genreDetailController", ["$scope", "Genres", "$modalInstance", "
 
     $scope.view = param.view; // sätter viken vy som ska visas.
     $scope.action = param.view; // talar om vilken handling användaren vill utföra i modalen
-    $scope.newGenre = {};
+    $scope.genre = {};
     // --- Hämtar genren ------ //
     $scope.$on("gotGenre", function (event, data) {
-        $scope.genre = data;
-     });
+        $scope.genre = $scope.orgGenre = data;
+    });
     Genres.get(param.id);
     // --- slut ---------------//
 
@@ -16,13 +16,39 @@ app.controller("genreDetailController", ["$scope", "Genres", "$modalInstance", "
     $scope.save = function () {
         Genres.put($scope.genre.Id, $scope.genre);
         $modalInstance.close();
+        console.log("$scope.action: ", $scope.action);
+        //console.log(action);
     };
-    
+
     $scope.cancel = function () {
         $modalInstance.dismiss('cancel');
     };
     //-------------------------//
 
+    // testar
+    $scope.text = "";
+    $scope.text2 = "";
+    $scope.editText = function () {
+        var a = $scope.action;
+        var text = "Vill du verkligen "
+        if (a == 1) text += "uppdatera "
+        if (a == 4) text += "skapa "
+        if (a == 3) text += "ta bort ";
+        text += "genre: "
+        //if (a != 4) text += genre.nametoUpperCase();
+
+        //if ($scope.action != 4){
+        var text2 = "";
+        if (a != 3) text2 = "med foljande innehall?"
+        if (a == 3) text2 = "och alla dess referenser permanent?";
+        if (a == 4) text2 = "med dessa uppgifter?";
+        $scope.text2 = text2;
+
+        //-------------
+        console.log(text);
+        $scope.text = text;
+    }
+    console.log($scope.confirmText1);
     //---------skapa ny -------//
     $scope.create = function () {
         console.log($scope.newGenre)
@@ -34,14 +60,14 @@ app.controller("genreDetailController", ["$scope", "Genres", "$modalInstance", "
     // ----- Delete ------------------- //
 
     $scope.delete = function () {
-            try {
-                Genres.delete($scope.genre.Id);
-                //alert("Genren" + $scope.genre.Name + "och dess referenser är borttagna.");
-            }
-            catch (err) {
-                //alert("Något gick fel:  " + err);
-            }
-            $modalInstance.close();
+        try {
+            Genres.delete($scope.genre.Id);
+            //alert("Genren" + $scope.genre.Name + "och dess referenser är borttagna.");
+        }
+        catch (err) {
+            //alert("Något gick fel:  " + err);
+        }
+        $modalInstance.close();
     };
 
     //---------Slut delete -------------//
@@ -56,11 +82,11 @@ app.controller("genreDetailController", ["$scope", "Genres", "$modalInstance", "
      *  4 - skapa nytt
      * 
      */
-      
+
     $scope.editView = function (id) {
-            if (id == 1) { $scope.view = [id]; }
-            if (id == 0) { $scope.view = [id]; }
-            if (id == 2) { $scope.view = [id]; }
+        if (id == 1) { $scope.view = [id]; }
+        if (id == 0) { $scope.view = [id]; }
+        if (id == 2) { $scope.view = [id]; }
     };
     // --- slut ---------------------//
 
