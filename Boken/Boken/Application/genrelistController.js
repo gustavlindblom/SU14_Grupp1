f75a03af60be36c992cf44a11a70b3514b3da953
glindblom <lindblom.gustav@gmail.com>
2015-03-26 14:33:16 +0100

    Cleaned up genrelistController

--- Boken/Boken/Application/genrelistController.js
+++ Boken/Boken/Application/genrelistController.js
@@ -1,26 +1,25 @@
 app.controller("genrelistController", ["$scope", "Genres", "$modal", "$log", "$route", function ($scope, Genres, $modal, $log, $route) {
-
-    // -- Hämtar lista med genres -- //
-    $scope.$on("gotGenres", function (event, data) {
-        //console.log("gotGenres triggered: ", data);
-        $scope.output = JSON.stringify(data, null, '\t');
-        $scope.genres = data;
-        $scope.totalItems = $scope.genres.length;
-        $scope.pagArr = $scope.genres.slice($scope.startshow, $scope.endshow);
-    });
-    Genres.get();
-
-    $scope.$on("reloadList", function (event, data) {
-        $route.reload();
-    });
-
-    //--- Början på paginering ---------//
+    // Pagination properties
     $scope.pagArr = [];
     $scope.bigCurrentPage = 1;
     $scope.itemsPP = 5;
     $scope.startshow = ($scope.bigCurrentPage - 1) * $scope.itemsPP;
     $scope.endshow = ($scope.startshow + $scope.itemsPP);
 
+    // Fetch genres data from database
+    $scope.$on("gotGenres", function (event, data) {
+        $scope.genres = data;
+        $scope.totalItems = $scope.genres.length;
+        $scope.pagArr = $scope.genres.slice($scope.startshow, $scope.endshow);
+    });
+    Genres.get();
+
+    // Reload the list when changes occur
+    $scope.$on("reloadList", function (event, data) {
+        $route.reload();
+    });
+
+    // Splice the list of genres depending on current page
     $scope.pageChanged = function (currScope) {
         $scope.bigCurrentPage = currScope.bigCurrentPage;
         $scope.startshow = ($scope.bigCurrentPage - 1) * $scope.itemsPP;
@@ -28,9 +27,9 @@ app.controller("genrelistController", ["$scope", "Genres", "$modal", "$log", "$r
         $scope.pagArr = $scope.genres.slice($scope.startshow, $scope.endshow);
     }
 
-    // ----- Modal -------------------- //
+    // Open a modal view of a genre if the user clicks it, or wants to edit/create/delete
     $scope.open = function ( view,  genre, action ) {
-        if (genre) {
+        if (genre) { // the user wants to view an existing book, or edit/delete
             var modalInstance = $modal.open({
                 templateUrl: 'partials/genreDetail.html',
                 controller: 'genreDetailController',
@@ -51,7 +50,7 @@ app.controller("genrelistController", ["$scope", "Genres", "$modal", "$log", "$r
             }, function () {
             });
         }
-        if (!genre) {
+        if (!genre) { // The user wants to create a new book
              var modalInstance = $modal.open({
                     templateUrl: 'partials/genreDetail.html',
                     controller: 'genreDetailController',
