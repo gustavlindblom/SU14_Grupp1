5b54ed179870d2e7297ee6596fcfe55fa8e667c9
glindblom <lindblom.gustav@gmail.com>
2015-03-26 11:55:30 +0100

    Cleaned up homeController.js from comments and logs. Also made sure a books rating is removed when a book is removed.

--- Boken/Boken/Application/homeController.js
+++ Boken/Boken/Application/homeController.js
@@ -10,7 +10,6 @@ app.controller("homeController", ["$scope", "Books", "Ratings", "$modal", functi
     // lyssna på gotRatings
     $scope.$on("gotRatings", function (event, data) {
         $scope.ratings = data;
-        console.log(data);
         $scope.topThreeBooks = [];
         var topThreeRatings = data.sort(function (a, b) { return b.AverageRating - a.AverageRating; }).slice(0, 3);
         // bygger arrayen 3 topratade books
@@ -21,13 +20,9 @@ app.controller("homeController", ["$scope", "Books", "Ratings", "$modal", functi
                 }
             });
         });
-        console.log($scope.topThreeBooks);
-
     });
 
     $scope.open = function (view, book, action, size) {
-        console.log("book", book, "view", view);
-
         if (book) {
             var modalInstance = $modal.open({
                 templateUrl: 'partials/bookDetail.html',
@@ -40,13 +35,11 @@ app.controller("homeController", ["$scope", "Books", "Ratings", "$modal", functi
                             view: view,
                             action: action
                         }
-                        console.log("param:", params)
                         return params;
                     }
                 }
             });
             modalInstance.result.then(function (selectedItem) {
-                //$route.reload();
                 $scope.selected = selectedItem;
             }, function () {
 
@@ -56,7 +49,6 @@ app.controller("homeController", ["$scope", "Books", "Ratings", "$modal", functi
             var modalInstance = $modal.open({
                 templateUrl: 'partials/bookDetail.html',
                 controller: 'bookDetailController',
-                //size: size,
                 resolve: {
                     param: function () {
                         params = {
@@ -70,7 +62,6 @@ app.controller("homeController", ["$scope", "Books", "Ratings", "$modal", functi
             });
 
             modalInstance.result.then(function (selectedItem) {
-                console.log("Kommer vi hit någon  gång gång?");
                 $route.reload();
                 $scope.selected = selectedItem;
             }, function () {
@@ -80,24 +71,6 @@ app.controller("homeController", ["$scope", "Books", "Ratings", "$modal", functi
 
 
     };
-    //var imagePath = '/Content/Image/';
-
-    //var images = [imagePath + 'saganomringen.jpg', imagePath + 'anglarodemoner.jpg', imagePath + 'davinci.jpg', imagePath + 'Det.jpg'];
-
-
-    //$scope.myInterval = 5000;
-    //var slides = $scope.slides = [];
-
-    //$scope.addSlide = function () {
-    //    var newWidth = slides.length + 1;
-    //    slides.push({
-    //        image: images[i],
-
-    //    });
-    //};
-    //for (var i = 0; i < 4; i++) {
-    //    $scope.addSlide();
-    //}
 
     Books.get();
 }]);
