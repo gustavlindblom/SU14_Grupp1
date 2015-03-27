2afb3416910ffb6582c9b303822feb993cd7b7a6
glindblom <lindblom.gustav@gmail.com>
2015-03-27 11:57:16 +0100

    Fixed route config and added separation in the lists.

--- Boken/Boken/App_Start/RouteConfig.cs
+++ Boken/Boken/App_Start/RouteConfig.cs
@@ -12,10 +12,11 @@ namespace Boken
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{staticfile}", new { staticfile = @".*\.(css|js|gif|jpg|jpeg)(/.*)?" });
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{*anything}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
